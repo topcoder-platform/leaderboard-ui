b35380a95e8cd00fc46589c871aa4de5b5aa3369
Dushyant <dushyantbhalgami@gmail.com>
2018-11-02 13:43:03 +0530

    added missing pages

---
+++ components/MyLayout.js
@@ -0,0 +1,14 @@
+const layoutStyle = {
+    margin: 20,
+    padding: 20,
+    border: '1px solid #DDD'
+  }
+  
+  const Layout = (props) => (
+    <div style={layoutStyle}>
+      <Header />
+     
+    </div>
+  )
+  
+  export default Layout
