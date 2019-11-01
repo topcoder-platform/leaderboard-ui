24d8b514847ab1cd7cd37c8dec8752041639c7ed
dat <dat.nguyen.supper@gmail.com>
2019-11-01 23:55:45 +0700

    #42 Winners Page | Update page to match design
    #42 Winners Page | Update page to match design

--- components/PageHead.js
+++ components/PageHead.js
@@ -7,6 +7,7 @@ export default () => (
       <meta name='viewport' content='initial-scale=1.0, width=device-width, shrink-to-fit=no' />
       <link href='https://fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800' rel='stylesheet' />
       <link href='https://fonts.googleapis.com/css?family=Barlow+Condensed:300,400,500,700' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Barlow&display=swap' rel='stylesheet' />
       <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' />
     </Head>
     <style jsx global>{`
@@ -18,7 +19,7 @@ export default () => (
         margin: 0;
         padding: 0;
         height: 100%;
-        font-family: 'Montserrat', sans-serif;  
+        font-family: 'Montserrat', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         background: #02142B;
